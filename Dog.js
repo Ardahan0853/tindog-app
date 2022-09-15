e3f1d144c4ce7ddb8922f35deecc0d347499ba0b
Ardahan Özduman <ardahan08530853@hotmail.com>
2022-09-15 16:17:18 +0300

    first commit

---
+++ Dog.js
@@ -0,0 +1,27 @@
+
+
+class newDow {
+    constructor(data){
+        Object.assign(this,data)
+    }
+    getDogHtml(){
+        const {name,  age, bio } = this
+        
+        
+        return (
+
+
+            `<div class="background-image" id="image">
+                <div class="rotated" id="rotated-id">
+                    <img src="./images/badge-like.png"></img>
+                </div>
+                <div class="d-flex-image">
+                    <h1>${name}, ${age}</h1>
+                    <h2>${bio}</h2>
+                </div>
+            </div>`
+        )
+    }
+}
+
+export default newDow
---
+++ index.js
@@ -0,0 +1,60 @@
+import dogs from "./data.js"
+import newDow from "./Dog.js"
+
+
+let numberArray = [0, 1, 2]
+
+
+function getNewDog(){
+    let nextDogData = dogs[numberArray.shift()]
+    return nextDogData ? new newDow(nextDogData) : {}
+    
+    
+}
+
+
+const firstDog = new newDow(dogs[0])
+
+let  otherDogs = getNewDog()
+
+
+function render(){
+    
+    document.getElementById("image-div").innerHTML = firstDog.getDogHtml()
+    image.style.backgroundImage = `url(${firstDog.avatar})`
+
+}
+
+render()
+
+function render2(){
+    document.getElementById("image-div").innerHTML = otherDogs.getDogHtml()
+    image.style.backgroundImage = `url(${otherDogs.avatar})`
+    
+}
+
+
+
+
+document.getElementById("like").addEventListener("click", function(){
+    
+    const rotated = document.getElementById("rotated-id")
+    rotated.style.opacity = "1"
+    setTimeout(()=>{
+        otherDogs = getNewDog()
+        render2()
+        
+    }, 2000)
+})
+
+document.getElementById("dislike").addEventListener("click",function(){
+    const rotated = document.getElementById("rotated-id")
+    
+    rotated.style.opacity = "1"
+    rotated.innerHTML = `<img src="./images/badge-nope.png"></img>`
+    setTimeout(()=>{
+        otherDogs = getNewDog()
+        render2()
+        
+    }, 2000)
+} )
